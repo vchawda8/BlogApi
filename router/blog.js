bbbb3066a285f9de11ddca8d6aa0a3f13ef8adc3
vchawda8 <vchawda8@gmail.com>
2017-12-19 10:26:42 +0530

    changes in blog router

--- router/blog.js
+++ router/blog.js
@@ -4,23 +4,31 @@
  * @description registering routes that will serve any users request
  */
 
- const blog = require('./../controller/blog')
+const blog = require('./../controller/blog')
 
 const blogRoutes = [{
+
     method: 'POST',
-    path: '/blog',
-    config:{
-        auth:'token',
+    path  : '/blog',
+    config: {
+
+        auth   : 'token',
         handler: blog.addBlogPost
+
     }
+
 }, {
-    method: 'GET',
-    path: '/blog',
-    handler:blog.getAllBlogPost
+
+    method : 'GET',
+    path   : '/blog',
+    handler: blog.getAllBlogPost
+
 }, {
-    method: 'GET',
-    path: '/blog/{blogId}',
-    handler:blog.getOneBlogPost
+
+    method : 'GET',
+    path   : '/blog/{blogId}',
+    handler: blog.getOneBlogPost
+
 }]
 
 module.exports = blogRoutes;
